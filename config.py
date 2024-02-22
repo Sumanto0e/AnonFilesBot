fccebbf3e477f847fe5735c1787c794d3226fbc9
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-02-22 20:13:04 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6995744028:AAEfAuJkdreorU_OsWsRU_w7MFOxQifbDJY")
 
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", "20244703"))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "aecd498f68e85c1c925a6a9f95103aee")
 
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "onsbase")
